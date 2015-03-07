5e8d4af85bbff1acab3343fdd6049abf339c3e5d
dev-strender <developer.strender@gmail.com>
2015-03-07 20:49:37 +0900

    added config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+#enter your facebook email
+fb_email = 'test@example.com'
+
+#enter your facebook password
+fb_pass = 'passwordexample'
+
+#enter your application id. You have to make an app to get access token.
+app_id = 123456789012345
+
+#enter the id number that you want to scrap in. my case is scrapping feeds in the group
+group_id = 123456789012345
+
+#you can limit the maximum size of the content. the script will scrap at most that size of limit from the recent post to older post
+limit = 1000
